a9025fe1e28fba0fdd272f5565333784d7091354
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-15 22:28:46 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -3,25 +3,27 @@ import java.util.ArrayList;
 public class Digits
 {
 
-	private ArrayList<Integer> digitList;
+  private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	  digitList = new ArrayList<Integer>();
-	  if(num == 0) digitList.add(0); 
-	  for(int i = num; i > 0; i/=10){
-	   digitList.add(i%10); 
-	  }
-	}
+  public Digits(int num)
+  { /* to be implemented in part (a) */ 
+    digitList = new ArrayList<Integer>();
+    if(num == 0) digitList.add(0); 
+    int n = num ;
+    while(n > 0 ){
+     digitList.add(0, n%10);
+     n=n/10; 
+    }    
+  }
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		for(int i = 0; i < digitList.size()-1; i++) if(digitList.get(i+1) <= digitList.get(i)) return false; 		
- 	 return true; 
-	}
-	
-	public String toString()
-	{
-		return digitList.toString();
-	}
+  public boolean isStrictlyIncreasing()
+  { /* to be implemented in part (b) */
+    for(int i = 0; i < digitList.size()-1; i++) if(digitList.get(i+1) <= digitList.get(i)) return false;     
+    return true; 
+  }
+  
+  public String toString()
+  {
+    return digitList.toString();
+  }
 }
