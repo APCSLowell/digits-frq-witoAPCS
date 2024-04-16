0b5c6727ffe1f9f307d20c9568adc47f2aaebe7e
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-15 21:52:39 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,13 +7,18 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	  ArrayList<Integer> balls = new ArrayList<Integer>();	  
+          for(int i = num; i > 0; i/= 10) balls.add(i%10);
+	  digitList = new ArrayList<Integer>();
+	  for(int i = balls.size()-1; i >= 0; i--){
+	    digitList.add(balls.get(i)); 
+	  }	    
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
-
+		for(int i = 0; i < digitList.size()-1; i++) if(digitList.get(i+1) <= digitList.get(i) return false; 		
+ 	 return true; 
 	}
 	
 	public String toString()
