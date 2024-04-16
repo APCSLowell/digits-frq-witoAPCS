1e7a6a7ac400e69d7cf02e725e5c31f328902e9b
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-04-15 21:54:06 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -17,7 +17,7 @@ public class Digits
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		for(int i = 0; i < digitList.size()-1; i++) if(digitList.get(i+1) <= digitList.get(i) return false; 		
+		for(int i = 0; i < digitList.size()-1; i++) if(digitList.get(i+1) <= digitList.get(i)) return false; 		
  	 return true; 
 	}
 	
